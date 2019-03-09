ade4367fb6bfdec9cb079dc27a3d2ee010f28dc1
Sugaryesplz7 <hanimichi@hotmail.com>
2019-03-09 19:17:13 +0800

    Lesson 1 finished

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,4 @@
+''''
 class Student():
     def __init__(self, name):
         self._name = name
@@ -13,3 +14,37 @@ stu1 = Student("one")
 stu2 = Student("two")
 
 print(stu1 > stu2)
+'''
+
+class Person:
+    # 实例方法
+    def eat(self):
+        print(self)
+        print("Eating.....")
+
+    # 类方法
+    # 类方法的第一个参数，一般命名为cls，区别于self
+    @classmethod
+    def play(cls):
+        print(cls)
+        print("Playing.....")
+
+    # 静态方法
+    # 不需要用第一个参数表示自身或者类
+    @staticmethod
+    def say():
+        print("Saying....")
+
+
+yueyue = Person()
+
+# 实例方法
+yueyue.eat()
+#名称是 person object
+# 类方法
+Person.play()
+#名称是Person
+yueyue.play()
+# 静态方法
+Person.say()
+yueyue.say()
